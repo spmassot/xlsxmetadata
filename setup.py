d062fbac83e3ec7f6357077fc5bcd7716e8b18bb
spmassot <sean.massot@namely.com>
2018-08-07 14:43:27 -0400

    Bakes in BytesIO logic for reading data from FileStorage objects

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='xlsxmetadata',
-    version='0.0.2',
-    author='automatonymous',
+    version='0.0.3',
+    author='spmassot',
     author_email='spmassot@gmail.com',
     description='Really lightweight lib for peeking into xlsx column/row size before you try to open the file with something else',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/automatonymous/xlsxmetadata',
+    url='https://github.com/spmassot/xlsxmetadata',
     packages=['xlsxmetadata'],
     zip_safe=False
 )
